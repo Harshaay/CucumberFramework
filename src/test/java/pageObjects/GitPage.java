832f42fc911453d8ff0b41f3b1d5b4ef206f2c27
Harsha <harshaay1495@gmail.com>
2025-04-08 00:49:35 +0530

    git practice file add

--- src/test/java/pageObjects/GitPage.java
+++ src/test/java/pageObjects/GitPage.java
@@ -6,7 +6,14 @@ public class GitPage {
 	@Test
 	public void gitPractice()
 	{
-		System.out.println("GitDemo");
+		System.out.println("GitDemo1");
+		System.out.println("GitDemo2");
+		System.out.println("GitDemo3");
+		System.out.println("GitDemo4");
 	}
-
+	
+	public void gitPractice2()
+	{
+		System.out.println("gitPractice2");
+	}
 }
